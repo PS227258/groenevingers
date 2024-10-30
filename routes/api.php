a2cff18a7a949075c80d1594312e07d2f9525576
PS227258 <ps227258@summacollege.nl>
2024-10-30 16:50:15 +0100

    updated route names and workflow

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ApiOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Spatie\FlareClient\Api;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/orders', ApiOrderController::class)->only(['index', 'show']);
+Route::get('/orders', [ApiOrderController::class, "index"])->name("api_orders.index");
+Route::get('/orders/{order}', [ApiOrderController::class, "show"])->name("api_orders.show");
